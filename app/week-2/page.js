df2b228cd5855aae98febe6367fcdf74b35256ad
ParvPatel <parvpradipbhai.patel@edu.sait.ca>
2024-09-23 17:36:57 -0600

    studentinfo

--- app/week-2/page.js
+++ app/week-2/page.js
@@ -1,7 +1,10 @@
+import studentinfo from "./student-info"
+
 export default function Page() {
     return (
       <main>
         <h1>Shopping List</h1>
+        <student-info />
       </main>
     );
   }
---
+++ app/week-2/student-info.js
@@ -0,0 +1,11 @@
+
+import Link from "next/link";
+export default function StudentInfo() {
+    return (
+      <main>
+        <h1>Student Name</h1>
+        <p>Parv Patel</p>
+        <Link href={"https://github.com/ParvPatel6/cprg306-assignments"}>My Github Repository</Link>
+      </main>
+    );
+  }
